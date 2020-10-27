3f825e11f16704bae91dd957138a320824422df2
jfitzsimmons <jfitzhome@gmail.com>
2020-10-27 08:22:31 -0500

    updated dependencies, eslint config

---
+++ src/components/Scream.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import withStyles from '@material-ui/core/styles/withStyles';
+import { Link } from 'react-router-dom';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+// MUI
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+//import AppBar from '@material-ui/core/AppBar';
+
+const styles = {
+  card: {
+    display: 'flex',
+    marginBottom: 20,
+  },
+  image: {
+    width: 200,
+  },
+  content: {
+    padding: 25,
+    objectFit: 'cover',
+  }
+}
+
+ class Scream extends Component {
+  render() {
+    dayjs.extend(relativeTime);
+    const { classes, scream : { body, createdAt, userImage, userHandle, screamId, likeCount, commentCount } } = this.props;
+    return (
+      <Card className={classes.card}>
+        <CardMedia
+        className={classes.image}
+        image={userImage}
+        title="Profile Image" />
+        <CardContent className={classes.content}>
+          <Typography color="primary" variant="h5" component={Link} to={`/users/${userHandle}`}>{userHandle}</Typography>
+          <Typography variant="body2" color="textSecondary">{dayjs(createdAt).fromNow()}</Typography>
+          <Typography variant="body1" color="textPrimary">{body}</Typography>
+        </CardContent>
+      </Card>
+    )
+  }
+}
+
+export default withStyles(styles)(Scream);
---
+++ src/pages/home.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import {Grid} from '@material-ui/core';
+import Scream from '../components/Scream';
+
+export class Home extends Component {
+  state = {
+    screams: null
+  }
+  componentDidMount() {
+    axios.get('/screams')
+      .then(res => {
+        this.setState({
+          screams: res.data
+        })
+      })
+      .catch(err => console.error(err));
+  }
+  render() {
+    let recentScreamsMarkup = this.state.screams ? (
+      this.state.screams.map((scream) => <Scream key={scream.screamId} scream={scream} />)
+    ) : (
+      <p>Loading...</p>
+    );
+
+    return (
+      <Grid container spacing={3}>
+        <Grid item sm={8} xs={12}>
+          {recentScreamsMarkup}
+        </Grid>
+        <Grid item sm={4} xs={12}>
+          profile
+        </Grid>
+      </Grid>
+    )
+  }
+}
+
+export default Home
